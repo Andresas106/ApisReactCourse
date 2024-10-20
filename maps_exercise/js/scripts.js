07596b4b0c42b591a4a5ec1bb00e90dd2f446378
Andresas <averdes12@gmail.com>
2024-10-20 14:51:08 +0200

    Exercise done have to add css to it

--- maps_exercise/js/scripts.js
+++ maps_exercise/js/scripts.js
@@ -1,23 +1,49 @@
-/*
-function initMapa(){
-    
-    // Call to constructor V1
-    let mapa = new myMap();
-    
+
+var mapa;
+
+function initData()
+{
+    mapa = new myMap('https://citmalumnes.upc.es/~davids/awug2425/05%20maps/petrol/llista.php');
 }
 
-class myMap {
-    
-    //Constructor V1: Static id canvas element
-    constructor() {
-        this.forecast = [];
+function onClick24hs(){
+    mapa = new myMap('https://citmalumnes.upc.es/~davids/awug2425/05%20maps/petrol/llista24hs.php');
+    var details = document.getElementsByClassName('details')[0];
+    details.innerHTML = '';
+}
+
+function onClickAll(){
+    mapa = new myMap('https://citmalumnes.upc.es/~davids/awug2425/05%20maps/petrol/llista.php');
+    var details = document.getElementsByClassName('details')[0];
+    details.innerHTML = '';
+
+}
+
+class myMap{
+    constructor(url){
         this.map = null;
+        this.forecast = [];
+        this.url = url;
         this.drawMap();
+        
     }
 
     drawMap() {
+        const mapContainer = document.getElementById('map');
+        
+        if (this.map !== null) {
+            this.map.remove(); // Destruye el mapa existente
+        }
+
+        // Elimina cualquier mapa Leaflet anterior si existe
+        if (mapContainer._leaflet_id) {
+            mapContainer._leaflet_id = null;  // Restablece el ID de Leaflet
+            mapContainer.innerHTML = '';  // Limpia el contenedor
+        }
+
+
         this.map = L.map("map").setView([41.3887900,2.1589900], 13); // Constructor V1, static id 
-        //this.map = L.map(this.canvasId).setView([41.3887900,2.1589900], 13); // Constructor V2, canvas' id retrived from class prop
+        
 
         //Tiles
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -25,87 +51,23 @@ class myMap {
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(this.map);
 
-        console.log(this.map);
-
-        this.fetchData();
-    }
-
-    fetchData() {
-        fetch('https://citmalumnes.upc.es/~davids/awug2425/05%20maps/meteo_data/meteodata.php')
-        .then(response => {
-            return response.text();
-        })
-        .then(data => {   // Usage of arrow funtions to avoid losing context when using "this"  https://www.w3schools.com/js/js_arrow_function.asp
-            //console.log('data = ', data);
-            const parsedData = JSON.parse(data);
-            //console.log(parsedData.ciutats);
-            this.forecast = parsedData.ciutats;
-            //console.log(this.forecast[0]);
-            this.drawData();
-        })
-        .catch(err => {
-            console.error('Fetch error: ', err);
-        });        
-    }
-        
-    drawData(){
-        // Empty array to store coords/points list
-        const bounds = [];
-
-        this.forecast.forEach(city => {
-            console.log(city);
-            console.log(city.latitud);
-            this.drawMarker(city.latitud, city.longitud, city.prediccio);
-            
-            // Adding marker coords to the list for fitbounds
-            bounds.push([city.latitud, city.longitud]);   
-        });
-
-        console.log(bounds);
-        //Fit bounds
-        if (bounds.length) {
-            const latLngBounds = L.latLngBounds(bounds);
-            this.map.fitBounds(latLngBounds);
-        }
-    }
-
-    drawMarker(lat, lon, prev) {
-        // Icon for marker
-        const customIcon = L.icon({
-            iconUrl: `https://citmalumnes.upc.es/~davids/awug2425/05%20maps/meteo_data/imagenes/${prev}.png`, //Icon source with template literal https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-            iconSize: [32,32], // Icon size
-            iconAnchor: [16, 16] // Point of incon to align with marker coords
-        });
-        
-        // Adding marker
-        L.marker([lat,lon], { icon: customIcon }).addTo(this.map);
-    }
-}
-    */
-
-function initData()
-{
-    let mapa = new myMap();
-}
-
-class myMap{
-    constructor(){
         this.fetchList();
     }
 
     fetchList(){
-        fetch('https://citmalumnes.upc.es/~davids/awug2425/05%20maps/petrol/llista.php')
+        fetch(this.url)
         .then(response => {
             return response.text();
         })
         .then(data => {   // Usage of arrow funtions to avoid losing context when using "this"  https://www.w3schools.com/js/js_arrow_function.asp
             //console.log('data = ', data);
             const parsedData = JSON.parse(data);
+            this.forecast = parsedData.benzineres;
             //console.log(parsedData.ciutats);
 
             const petrols = parsedData.benzineres;
-            console.log(parsedData.benzineres);
             var data_list = document.getElementById('list');
+            data_list.innerHTML = '';
 
 
             petrols.forEach(element => {
@@ -118,11 +80,17 @@ class myMap{
 
                 let pText = document.createTextNode(element.nom + " (" + element.adreca + ")");
 
+                button.addEventListener('click', () =>{
+                    this.showDetails(element);
+                });
+
                 p.appendChild(pText);
                 div.appendChild(p);
                 div.appendChild(button);
                 data_list.appendChild(div);
             });
+
+            this.drawData();
             
 
             
@@ -130,5 +98,80 @@ class myMap{
         .catch(err => {
             console.error('Fetch error: ', err);
         });     
+    }
+
+    drawData(){
+        // Empty array to store coords/points list
+        const bounds = [];
+
+        this.forecast.forEach(petrol => {
+
+
+            this.drawMarker(petrol.lat, petrol.lon, petrol);
+            
+            // Adding marker coords to the list for fitbounds
+            bounds.push([petrol.lat, petrol.lon]);   
+        });
+        //Fit bounds
+        if (bounds.length) {
+            const latLngBounds = L.latLngBounds(bounds);
+            this.map.fitBounds(latLngBounds);
+        }
+    }
+
+    drawMarker(lat, lon, petrol) {
+        const marker = L.marker([lat,lon]).addTo(this.map);
+
+        marker.on('click', () => {
+            this.showDetails(petrol);    
+        });
+    }
+
+    showDetails(petrol)
+    {
+        fetch('https://citmalumnes.upc.es/~davids/awug2425/05%20maps/petrol/benzinera.php?benzinera=' + petrol.id)
+        .then(response => {
+            return response.text();
+        })
+        .then(data => {   // Usage of arrow funtions to avoid losing context when using "this"  https://www.w3schools.com/js/js_arrow_function.asp
+            const parsedData = JSON.parse(data);
+            console.log(parsedData);
+
+            var details = document.getElementsByClassName('details')[0];
+            details.innerHTML = '';
+
+            var pname = document.createElement('p');
+            var ptextname = document.createTextNode('Name: ' + parsedData.benzinera.nom + '(' + parsedData.benzinera.adreca + ')');
+            pname.appendChild(ptextname);
+            details.appendChild(pname);
+
+
+            var ptel = document.createElement('p');
+            var ptexttel = document.createTextNode('Phone: ' + parsedData.benzinera.telefon);
+            ptel.appendChild(ptexttel);
+            details.appendChild(ptel);
+
+            if(parsedData.benzinera.email != '')
+            {
+                var pemail = document.createElement('p');
+                var ptextemail = document.createTextNode('Email: ' + parsedData.benzinera.email);
+                pemail.appendChild(ptextemail);
+                details.appendChild(pemail);
+            }
+
+            var pschedule = document.createElement('p');
+            var ptextschedule = document.createTextNode('Schedule: ' + parsedData.benzinera.horari);
+            pschedule.appendChild(ptextschedule);
+            details.appendChild(pschedule);
+
+            
+            
+            
+
+            
+        })
+        .catch(err => {
+            console.error('Fetch error: ', err);
+        });    
     }
 }
